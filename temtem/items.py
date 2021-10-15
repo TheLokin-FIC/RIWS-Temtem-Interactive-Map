89f5202a05485a33302367ff62bda1ed3a6e730e
Iván Barrientos Lema <ivan.barrientos.lema@gmail.com>
2021-10-15 18:02:16 +0200

    Added locations

--- temtem/items.py
+++ temtem/items.py
@@ -1,8 +1,16 @@
 from scrapy import Field, Item
 
 
+class Locations(Item):
+    routes = Field()
+    islands = Field()
+    rarities = Field()
+    levels = Field()
+
+
 class TemtemItem(Item):
     number = Field()
     name = Field()
     types = Field()
     portrait = Field()
+    locations = Field()
--- temtem/spiders/temtems.py
+++ temtem/spiders/temtems.py
@@ -1,26 +1,60 @@
 from scrapy import Spider
-from temtem.items import TemtemItem
+from temtem.items import Locations, TemtemItem
 
 
 class TemtemSpider(Spider):
-    name = 'temtem'
-    start_urls = ['https://temtem.fandom.com/wiki/Temtem_(creatures)']
+    name = "temtem"
+    start_urls = ["https://temtem.fandom.com/wiki/Mimit"]
+
+    # def parse(self, response):
+    #    links = response.css(
+    #        "table.temtem-list > tbody > tr > td > span > a::attr(href)"
+    #    ).extract()
+    #    for link in links:
+    #        yield response.follow(link, self.parse_temtem)
 
     def parse(self, response):
-        links = response.css(
-            'table.temtem-list > tbody > tr > td > span > a::attr(href)').extract()
-        for link in links:
-            yield response.follow(link, self.parse_temtem)
-
-    def parse_temtem(self, response):
         temtem = TemtemItem()
-        temtem['number'] = response.xpath(
-            '//*[@id="mw-content-text"]/div/div[1]/table/tbody/tr[4]/td/text()').extract_first().rstrip()
-        temtem['name'] = response.xpath(
-            '//*[@id="mw-content-text"]/div/div[1]/table/tbody/tr[1]/th/text()').extract_first().rstrip()
-        temtem['types'] = response.xpath(
-            '//*[@id="mw-content-text"]/div/div[1]/table/tbody/tr[5]/td/a/@title').extract()
-        temtem['portrait'] = response.xpath(
-            '//*[@id="ttw-temtem"]/span/a/img/@src').extract()
+        locations = Locations()
+
+        locations["routes"] = []
+        locations["islands"] = []
+        locations["rarities"] = []
+        locations["levels"] = []
+
+        temtem["number"] = (
+            response.xpath(
+                '//*[@id="mw-content-text"]/div/div[1]/table/tbody/tr[4]/td/text()'
+            )
+            .extract_first()
+            .rstrip()
+        )
+        temtem["name"] = (
+            response.xpath(
+                '//*[@id="mw-content-text"]/div/div[1]/table/tbody/tr[1]/th/text()'
+            )
+            .extract_first()
+            .rstrip()
+        )
+        temtem["types"] = response.xpath(
+            '//*[@id="mw-content-text"]/div/div[1]/table/tbody/tr[5]/td/a/@title'
+        ).extract()
+        temtem["portrait"] = response.xpath(
+            '//*[@id="ttw-temtem"]/span/a/img/@src'
+        ).extract()
+
+        for attr in response.xpath("//table[7]/tbody/tr/td[1]/a/@title").extract():
+            locations["routes"].append(attr)
+
+        for attr in response.xpath("//table[7]/tbody/tr/td[2]/a/@title").extract():
+            locations["islands"].append(attr)
+
+        for attr in response.xpath("//table[7]/tbody/tr/td[3]/text()").extract():
+            locations["rarities"].append(attr.rstrip())
+
+        for attr in response.xpath("//table[7]/tbody/tr/td[4]/text()").extract():
+            locations["levels"].append(attr.rstrip())
+
+        temtem["locations"] = locations
 
         return temtem
